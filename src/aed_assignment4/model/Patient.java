bb141abcbcf474c8e811fc2033ca10ff217267fb
bhatia-di <bhatia.di@northeastern.edu>
2021-10-29 23:07:43 -0400

    added model info

---
+++ src/aed_assignment4/model/Patient.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aed_assignment4.model;
+
+/**
+ *
+ * @author 18577
+ */
+public class Patient {
+    
+    private String patientID;
+    private String primaryDoctorName;
+    private String prefferedPharmacy;
+
+    public String getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(String patientID) {
+        this.patientID = patientID;
+    }
+
+    public String getPrimaryDoctorName() {
+        return primaryDoctorName;
+    }
+
+    public void setPrimaryDoctorName(String primaryDoctorName) {
+        this.primaryDoctorName = primaryDoctorName;
+    }
+
+    public String getPrefferedPharmacy() {
+        return prefferedPharmacy;
+    }
+
+    public void setPrefferedPharmacy(String prefferedPharmacy) {
+        this.prefferedPharmacy = prefferedPharmacy;
+    }
+    
+}
---
+++ src/aed_assignment4/model/Person.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aed_assignment4.model;
+
+/**
+ *
+ * @author 18577
+ */
+public class Person {
+    
+    private String personId;
+    private String name;
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    private int age;
+    
+    
+    
+}
