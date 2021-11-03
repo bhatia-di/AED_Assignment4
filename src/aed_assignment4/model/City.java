4584822504b87b00b3d639fd71b9fb209e29080b
bhatia-di <bhatia.di@northeastern.edu>
2021-11-02 23:22:31 -0400

    code added to saearc'

--- src/aed_assignment4/model/City.java
+++ src/aed_assignment4/model/City.java
@@ -1,2 +1,14 @@
-package aed_assignment4.model;public class City {
+package aed_assignment4.model;
+
+public class City {
+
+    public String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
--- src/aed_assignment4/model/Community.java
+++ src/aed_assignment4/model/Community.java
@@ -1,2 +1,13 @@
-package aed_assignment4.model;public class Community {
+package aed_assignment4.model;
+
+public class Community extends City{
+    private String community;
+
+    public String getCommunity() {
+        return community;
+    }
+
+    public void setCommunity(String community) {
+        this.community = community;
+    }
 }
